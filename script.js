f3d7850ee27a6a937a6a68fed24f40a014a45054
Trent Blackman <trentablackman@gmail.com>
2020-10-21 20:17:03 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+storeItems.forEach(function(item){
+
+    if (item.inStock === true){
+        $(`#chooseMe`).append(`<div class = 'inStock'><span class = 'row'>$${item.price}</span> <span class = 'row'>${item.name}</span> <span class = 'row'>${item.details}</span></div>`);
+    }
+
+
+});
+
+const click = document.getElementById(`button`);
+
+click.addEventListener('click', function(){
+    $(`#contentContainer`).toggleClass(`darkMode`);
+    $(`.inStock`).toggleClass('darkMode2');
+    $('.header1').toggleClass('darkMode3');
+    $(`button`).toggleClass('darkMode4');
+
+});
